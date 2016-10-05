63d5e338bab0f2299b9110f5e4ef1fe03b0cb9e5
panforrest <guoqianp@gmail.com>
2016-10-05 01:17:29 -0400

    combineControllers

--- routes/api.js
+++ routes/api.js
@@ -4,69 +4,92 @@ var Profile = require('../models/Profile')
 var ProfileController = require('../controllers/ProfileController')
 var Place = require('../models/Place')
 var PlaceController = require('../controllers/PlaceController')
+var controllers ={
+	profile: ProfileController,
+	place: PlaceController 
+}
 
 router.get('/:resource', function(req, res, next){
 
 	var resource = req.params.resource
-
-	if (resource == 'profile') {
-
-		ProfileController.get(null)
-		.then(function(results){
-       	res.json({
-        	confirmation: 'success',
-        		results: results
-        	})
-       	    return
-		})
-		.catch(function(err){
-    		res.json({
-    			confirmation: 'fail',
-    			message: err
-    		})
-
-    		return			
+	var controller = controllers[resource]
+	controller.get(null)
+	.then(function(results){
+   	res.json({
+    	confirmation: 'success',
+    		results: results
+    	})
+   	    return
+	})
+	.catch(function(err){
+		res.json({
+			confirmation: 'fail',
+			message: err
 		})
 
-        // Profile.find(null, function(err, results){
-        // 	if (err) {
-        // 		res.json({
-        // 			confirmation: 'fail',
-        // 			message: err
-        // 		})
+		return			
+	})
 
-        // 		return
-        // 	} 
 
-        // 	res.json({
-        // 		confirmation: 'success',
-        // 		results: results
-        // 	})
 
-        // 	return
-        // })
+	// if (resource == 'profile') {
 
-	}
+	// 	ProfileController.get(null)
+	// 	.then(function(results){
+ //       	res.json({
+ //        	confirmation: 'success',
+ //        		results: results
+ //        	})
+ //       	    return
+	// 	})
+	// 	.catch(function(err){
+ //    		res.json({
+ //    			confirmation: 'fail',
+ //    			message: err
+ //    		})
 
-	if (resource == 'place') {
+ //    		return			
+	// 	})
 
-		PlaceController.get(null)
-		.then(function(results){
-       	res.json({
-        	confirmation: 'success',
-        		results: results
-        	})
-       	    return
-		})
-		.catch(function(err){
-    		res.json({
-    			confirmation: 'fail',
-    			message: err
-    		})
+ //        // Profile.find(null, function(err, results){
+ //        // 	if (err) {
+ //        // 		res.json({
+ //        // 			confirmation: 'fail',
+ //        // 			message: err
+ //        // 		})
 
-    		return			
-		})
-	}		
+ //        // 		return
+ //        // 	} 
+
+ //        // 	res.json({
+ //        // 		confirmation: 'success',
+ //        // 		results: results
+ //        // 	})
+
+ //        // 	return
+ //        // })
+
+	// }
+
+	// if (resource == 'place') {
+
+	// 	PlaceController.get(null)
+	// 	.then(function(results){
+ //       	res.json({
+ //        	confirmation: 'success',
+ //        		results: results
+ //        	})
+ //       	    return
+	// 	})
+	// 	.catch(function(err){
+ //    		res.json({
+ //    			confirmation: 'fail',
+ //    			message: err
+ //    		})
+
+ //    		return			
+	// 	})
+	// }		
 
 })
 
